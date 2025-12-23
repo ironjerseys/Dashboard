c8a4f368e3949fea2117f057494f5ff67126d34c
Joris Reynes <jo.r6349@gmail.com>
2025-12-23 21:58:22 +0100

    create objectif dans component a part

---
+++ Dashboard/Models/GoalFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard.Models;
+
+public class GoalFormModel
+{
+    public int? Id { get; set; }
+
+    [Required]
+    public string Titre { get; set; } = "";
+
+    public string? Description { get; set; }
+
+    [Required]
+    public DateOnly Debut { get; set; }
+
+    [Required]
+    public DateOnly Fin { get; set; }
+
+    public int? ArticleId { get; set; }
+    public bool IsDone { get; set; }
+}
