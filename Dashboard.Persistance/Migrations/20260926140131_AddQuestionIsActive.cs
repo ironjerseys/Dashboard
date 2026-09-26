e124e99f81971b0d967729fea95b0602e7c0ff29
ironjerseys <jo.r6349@gmail.com>
2026-09-26 10:05:47 -0400

    ok

---
+++ Dashboard.Persistance/Migrations/20260926140131_AddQuestionIsActive.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuestionIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "QuizQuestions",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "QuizQuestions");
+        }
+    }
+}
